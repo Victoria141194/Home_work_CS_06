f7cfa6894c015db17d24be415e65f476ddb7253f
Victoria <victoria.kainova@gmail.com>
2022-10-23 13:48:20 +0300

    Added tasks

---
+++ Task_41/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+int size = GetNumberFromUser("Введите количество элементов массива: ", "Ошибка ввода!");
+int[] array = FillArray(size);
+int count = GetPositiveCount(array);
+Console.Write(String.Join(" ", array));
+Console.WriteLine($" -> {count}");
+
+
+//...................................МЕТОДЫ...........................................
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!");
+    }
+    return array;
+}
+
+
+int GetPositiveCount(int[] array)
+{
+    count = 0;
+    foreach (int el in array)
+    {
+        if (el > 0) count++;
+    }
+    return count;
+}
---
+++ Task_43/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+int b1 = GetNumberFromUser("Введите целое значение b1: ", "Ошибка ввода!");
+int k1 = GetNumberFromUser("Введите целое значение k1: ", "Ошибка ввода!");
+int b2 = GetNumberFromUser("Введите целое значение b2: ", "Ошибка ввода!");
+int k2 = GetNumberFromUser("Введите целое значение k2: ", "Ошибка ввода!");
+double x = GetXCoord(b1, k1, b2, k2);
+double y = GetYCoord(k1, x, b1);
+Console.Write($"Точка пересечения двух прямых: {x}; {y}");
+
+
+
+//...................................МЕТОДЫ...........................................
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+
+double GetXCoord(int b1, int k1, int b2, int k2)
+{;
+    x = (double)(b2 - b1) / (k1 - k2);
+   
+    return x;
+}
+
+
+double GetYCoord(int k1, double x, int b1)
+{
+    y = (double) k1 * x + b1;
+    return y;
+}
+ 
